9794dbc272402a001f684baa50693407b9db0cf3
Bodata <github@bodata.nl>
2016-12-20 11:53:11 +0100

    Nog een poging...

--- lib/leaflet-measure/rails/engine.rb
+++ lib/leaflet-measure/rails/engine.rb
@@ -2,6 +2,9 @@ module Leaflet
   module Measure
     module Rails
       class Engine < ::Rails::Engine
+        initializer 'leaflet-measure.precompile' do |app|
+          app.config.assets.precompile += %w( leaflet-measure.js )
+        end
       end
     end
   end
--- lib/leaflet-measure/version.rb
+++ lib/leaflet-measure/version.rb
@@ -1,5 +1,5 @@
 module Leaflet
   module Measure
-    VERSION = "0.4.0"
+    VERSION = "0.4.1"
   end
 end
