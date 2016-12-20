ff125a9a7f4de67a3ffab3bb3f19179d8c5d2492
Bodata <github@bodata.nl>
2016-12-20 15:21:56 +0100

    Plaatjes nu ook erbij als asset. Versie naar 0.4.2

--- lib/leaflet-measure/rails/engine.rb
+++ lib/leaflet-measure/rails/engine.rb
@@ -4,6 +4,7 @@ module Leaflet
       class Engine < ::Rails::Engine
         initializer 'leaflet-measure.precompile' do |app|
           app.config.assets.precompile += %w( leaflet-measure.js )
+          app.config.assets.precompile += %w( cancel.png cancel_@2X.png check.png check_@2X.png focus.png focus_@2x.png rulers.png rulers_@2x.png start.png start_@2x.png trash.png trash_@2x.png )
         end
       end
     end
--- lib/leaflet-measure/version.rb
+++ lib/leaflet-measure/version.rb
@@ -1,5 +1,5 @@
 module Leaflet
   module Measure
-    VERSION = "0.4.1"
+    VERSION = "0.4.2"
   end
 end
