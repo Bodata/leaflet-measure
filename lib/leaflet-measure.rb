d56a006d18b52730a567619148ea734fbd857969
Bodata <github@bodata.nl>
2016-12-20 09:46:35 +0100

    rondje voor idea niet mee te nemen, cach leeggooien ?

--- lib/leaflet-measure.rb
+++ lib/leaflet-measure.rb
@@ -2,7 +2,9 @@ require "leaflet-measure/version"
 
 module Leaflet
   module Measure
-    class Engine < ::Rails::Engine
+    module Rails
+      class Engine < ::Rails::Engine
+      end
     end
   end
 end
